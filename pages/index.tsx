68d53a50028dbb84830c8c557d9d471d60d0d646
avizmarques <aviz.marques@gmail.com>
2023-12-16 15:57:53 +0100

    fix: build ts error

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,7 @@ export default function Home({
         data-testid="rooms-list"
         className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-5 mt-[72px]"
       >
-        {displayedRooms.map((room, i) => (
+        {displayedRooms.map(({ room, i }: { room: Room; i: number }) => (
           <RoomCard room={room} key={i} />
         ))}
       </ul>
