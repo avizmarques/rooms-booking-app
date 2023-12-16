01329513e11017b7ada520582e757c0a230fcb40
avizmarques <aviz.marques@gmail.com>
2023-12-16 16:36:51 +0100

    fix: render roomcard conditionally to attempt fixing of vercel build error

--- components/RoomCard.tsx
+++ components/RoomCard.tsx
@@ -6,30 +6,30 @@ import { Room } from "@/pages";
 export const RoomCard = ({ room }: { room: Room }) => {
   const { setBookingRoom } = useRooms();
 
-  return (
+  return Boolean(room) ? (
     <li>
       <button
         onClick={() => setBookingRoom(room)}
-        disabled={room?.spots === 0}
+        disabled={room.spots === 0}
         className="flex flex-col items-stretch w-full text-start group disabled:opacity-60"
       >
         <div className="aspect-[328/220] overflow-hidden rounded-xl relative">
           <Image
-            src={room?.thumbnail}
-            alt={room?.name}
+            src={room.thumbnail}
+            alt={room.name}
             fill
             priority
             sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, (max-width: 1280px) 33vw, 25vw"
             className={`object-cover rounded-[11px] relative ${
-              room?.spots !== 0 && "hover:scale-110 transition-all duration-500"
+              room.spots !== 0 && "hover:scale-110 transition-all duration-500"
             }`}
           />
         </div>
         <div className="flex items-start justify-between text-lg mt-3 pb-11">
           <div>
-            <h2 className="text-lg font-bold text-theme-black">{room?.name}</h2>
+            <h2 className="text-lg font-bold text-theme-black">{room.name}</h2>
             <p data-testid="spots-available" className="text-lg text-primary">
-              {room?.spots} spots remaining
+              {room.spots} spots remaining
             </p>
           </div>
 
@@ -39,5 +39,5 @@ export const RoomCard = ({ room }: { room: Room }) => {
         </div>
       </button>
     </li>
-  );
+  ) : null;
 };
