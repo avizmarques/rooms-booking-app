a6a22bbe28cefbd173773c7e0d33692d3cca40ac
avizmarques <aviz.marques@gmail.com>
2023-12-16 14:17:23 +0100

    feat: homepage with RoomCard list

---
+++ components/RoomCard.tsx
@@ -0,0 +1,30 @@
+import { Room } from "@/pages";
+import Image from "next/image";
+
+export const RoomCard = ({ room }: { room: Room }) => {
+  return (
+    <li>
+      <button className="flex flex-col items-stretch w-full text-start">
+        <div className="aspect-[328/220] overflow-hidden rounded-xl relative">
+          <Image
+            src={room.thumbnail}
+            alt={room.name}
+            fill
+            // sizes="" todo: optimization
+            className="object-cover rounded-[11px] relative"
+          />
+        </div>
+        <div className="flex items-start justify-between text-lg mt-3 pb-11">
+          <div>
+            <h2 className="text-lg font-bold text-theme-black">{room.name}</h2>
+            <p className="text-lg text-primary">{room.spots} spots remaining</p>
+          </div>
+
+          <div className="min-w-[90px] h-[29px] bg-primary rounded-[4px] text-white text-xs font-bold flex items-center justify-center">
+            Book!
+          </div>
+        </div>
+      </button>
+    </li>
+  );
+};
