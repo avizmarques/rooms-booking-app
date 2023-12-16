bf15e157676ae8813452dab7ae398383642a3600
avizmarques <aviz.marques@gmail.com>
2023-12-16 14:19:27 +0100

    style: hover styles on room card

--- components/RoomCard.tsx
+++ components/RoomCard.tsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 export const RoomCard = ({ room }: { room: Room }) => {
   return (
     <li>
-      <button className="flex flex-col items-stretch w-full text-start">
+      <button className="flex flex-col items-stretch w-full text-start group">
         <div className="aspect-[328/220] overflow-hidden rounded-xl relative">
           <Image
             src={room.thumbnail}
             alt={room.name}
             fill
             // sizes="" todo: optimization
-            className="object-cover rounded-[11px] relative"
+            className="object-cover rounded-[11px] relative hover:scale-110 transition-all duration-500"
           />
         </div>
         <div className="flex items-start justify-between text-lg mt-3 pb-11">
@@ -20,7 +20,7 @@ export const RoomCard = ({ room }: { room: Room }) => {
             <p className="text-lg text-primary">{room.spots} spots remaining</p>
           </div>
 
-          <div className="min-w-[90px] h-[29px] bg-primary rounded-[4px] text-white text-xs font-bold flex items-center justify-center">
+          <div className="min-w-[90px] h-[29px] bg-primary rounded-[4px] text-white text-xs font-bold flex items-center justify-center group-hover:bg-fuchsia-900 transition-colors duration-500">
             Book!
           </div>
         </div>
